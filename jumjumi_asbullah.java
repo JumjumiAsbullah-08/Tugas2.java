8bb1acf3485b0a96c288ad22b68eaafdf2a66e2c
Jumjumi Asbullah <76870885+JumjumiAsbullah-08@users.noreply.github.com>
2021-05-18 18:00:07 +0700

    Update jumjumi_asbullah.java

--- jumjumi_asbullah.java
+++ jumjumi_asbullah.java
@@ -1,1 +1,26 @@
-# Tugas2.java
+# Tugas dan Praktikum Java 
+# Dosen Pengampu :  MUHAMMAD DEDI IRAWAN, M,Kom
+
+package com.mycompany.jumjumi_asbullah.Jumjumi_asbullah;
+
+public class Jumjumi_Asbullah {
+    public static void main(String [] args){
+    System.out.println(" ===============");
+    System.out.println(" | J U M A S B |");
+    System.out.println(" ===============");
+    
+    byte J = 8;
+    short U = 8;
+    float M = 2001, A, S, B;
+    
+    A = J + U + M;
+    S = J * U * M;
+    B = J / U / M;
+    
+    System.out.println(" Hasil Penjumlahan Tgl + Bulan + Tahun Lahir adalah = " + A);
+    System.out.println(" Hasil Perkalian Tgl x Bulan x Tahun Lahir adalah = " + S);
+    System.out.println(" Hasil Pembagian Tgl / Bulan / Tahun Lahir adalah = " + B);
+    
+    System.out.println(" Dibuat Oleh : Prof. Jumjumi Asbullah");
+}
+}
